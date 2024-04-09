e22358b7a733e0e1a0d49fc3709be3ae74e047a7
easygsea <easygsea@gmail.com>
2024-04-09 13:57:35 -0700

    Delete modules/R in develop.

--- modules/R/03_data_manipulators.R
+++
@@ -1,7 +0,0 @@
-add_offset_to_start_position_and_drop_columns <- function(tibble) {
-  tibble %>%
-    # Add offset to start position. Subtract one as offset starts with 1.
-    mutate(position = probe_start_pos + offset - 1) %>%
-    # Select the required columns
-    select(library, probe_name, chromosome, position, depth, )
-}
